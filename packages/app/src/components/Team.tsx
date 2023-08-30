4401746c03cb1b97f9455bea48ae99391bcf387e
unknown <clovis@bdx.town>
2023-08-30 19:12:57 +0200

    WIP edit team

--- packages/app/src/components/Team.tsx
+++ packages/app/src/components/Team.tsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import { useAppState } from '../reducers/reducers';
 import { DatePicker } from './DatePicker';
 import { DateSpan, Person } from '@planning/lib';
-import { DateRange, Matcher } from 'react-day-picker';
+import { DateRange } from 'react-day-picker';
 
 
 export const Team = () => {
     const [dirty, setDirty] = React.useState(false);
     const { state } = useAppState();
+    const [team, setTeam] = React.useState(state.config.team)
     const [pickerAnchor, setPickerAnchor] = React.useState<HTMLElement>();
     const [dates, setDates] = React.useState<DateRange | undefined>();
+    const pickerCallback = React.useRef<null | ((e: DateRange) => void)>(null);
+
+    React.useEffect(() => {
+        setTeam(state.config.team);
+    }, [state.config.team])
 
 
     const onSubmit: React.FormEventHandler<HTMLFormElement> = React.useCallback((e) => {
@@ -18,15 +24,57 @@ export const Team = () => {
         setDirty(false);
     }, []);
 
-    const onPicker = React.useCallback((e: MouseEvent, span: DateSpan) => {
-        setDates({ from: span.start, to: span.end});
-        setPickerAnchor(e.currentTarget as HTMLElement);
-    }, []);
+    const onEditDateSpan = React.useCallback((person: Person, before: DateSpan, after: DateRange) => {
+        const index = team.findIndex((t) => t.name === person.name)
+        const wTeam = [...team];
+        const unavailable = person.unavailable.filter((d) => d.start.getTime() !== before.start.getTime() || d.end.getTime() !== before.end.getTime());
+        wTeam.splice(
+            index, 
+            1,
+            {
+               ...person,
+                unavailable: [...unavailable, { start: after.from as Date, end: after.to as Date}]
+            }
+        );
+        setTeam(wTeam);
+    }, [team]);
+
+    const onAddDateSpan = React.useCallback((person: Person, range: DateRange) => {
+        const index = team.findIndex((t) => t.name === person.name)
+        const wTeam = [...team];
+        wTeam.splice(
+            index, 
+            1,
+            {
+               ...person,
+               unavailable: [...person.unavailable, { start: range.from as Date, end: range.to as Date }]
+            }
+        );
+        setTeam(wTeam);
+    }, [team]);
 
     const onClosePicker = React.useCallback(() => {
         setPickerAnchor(undefined);
         setDates(undefined);
+        pickerCallback.current = null;
     }, []);
+
+    const onSelect = React.useCallback((range: DateRange | undefined) => {
+        setDates(range);
+        if(!pickerCallback.current || !range || !range.from || !range.to) return;
+        pickerCallback.current(range);
+    }, []);
+
+    const onPicker = React.useCallback((e: MouseEvent, person: Person, span: DateSpan | null) => {
+        setDates({ from: span?.start, to: span?.end});
+        setPickerAnchor(e.currentTarget as HTMLElement);
+        if(span) {
+            pickerCallback.current = (range: DateRange) => onEditDateSpan(person, span, range);
+        } else {
+            pickerCallback.current = (range: DateRange) => onAddDateSpan(person, range);
+        }
+    }, [onAddDateSpan, onEditDateSpan]);
+
 
     return (
         <>
@@ -50,17 +98,25 @@ export const Team = () => {
                             <td className='pb-2'>
                                 unavailable
                             </td>
+                            <td className='pb-2'>
+
+                            </td>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            state.config.team.map((person) => (
+                            team.map((person) => (
                                 <tr key={person.name}>
                                     <td className='p-1' >{ person.name }</td>
                                     <td className='p-1 flex items-center gap-2'>
                                         {
-                                            person.unavailable.map((span) => <button key={`${span?.start?.toLocaleDateString()}-${span?.end?.toLocaleDateString()}`} type="button" onClick={(e: unknown) => onPicker(e as MouseEvent, span)}>{ span.start.toLocaleDateString() } - { span.end.toLocaleDateString() }</button>)
+                                            person.unavailable.map((span) => <button key={`${span?.start?.toLocaleDateString()}-${span?.end?.toLocaleDateString()}`} type="button" onClick={(e: unknown) => onPicker(e as MouseEvent, person, span)}>{ span.start.toLocaleDateString() } - { span.end.toLocaleDateString() }</button>)
                                         }
+                                    </td>
+                                    <td>
+                                        <button type="button" onClick={(e: unknown) => onPicker(e as MouseEvent, person, null)}>
+                                            Add
+                                        </button>
                                     </td>
                                 </tr>
                             ))
@@ -69,7 +125,7 @@ export const Team = () => {
                 </table>
             </form>
             {
-                pickerAnchor && ReactDOM.createPortal(<DatePicker onClose={onClosePicker} key={`${dates?.from?.toLocaleDateString()}-${dates?.to?.toLocaleDateString()}`} mode="range" selected={dates} defaultMonth={dates?.from} numberOfMonths={dates?.from?.getMonth() !== dates?.to?.getMonth() ? 2 : 1} anchor={pickerAnchor} />, document.body)
+                pickerAnchor && ReactDOM.createPortal(<DatePicker onClose={onClosePicker} key={`${dates?.from?.toLocaleDateString()}-${dates?.to?.toLocaleDateString()}`} showOutsideDays fixedWeeks mode="range" selected={dates}  onSelect={onSelect} defaultMonth={dates?.from} numberOfMonths={2} anchor={pickerAnchor} />, document.body)
             }
         </>
     )
